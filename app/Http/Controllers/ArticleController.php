2c9cc3368d949bd7481ac0f99a6f4f39a1594344
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-01-21 10:37:33 -0500

    Arreglo de la rama

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,36 +6,61 @@ use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
-    private static $rules=[
-        'title' => 'required|string|unique:articles|max:255',
-        'body' => 'required',
-    ];
-    private static $messages=[
+    public static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
-        'body.required'=>'El body no es válido',
     ];
+
     public function index()
     {
-        return new ArticleCollection(Article::paginate(25));
+        return response()->json(new ArticleCollection(Article::paginate(5)));
     }
+
     public function show(Article $article)
     {
-        return response()->json(new ArticleResource($article),200);
+        return new ArticleResource($article);
     }
+
+    public function image(Article $article)
+    {
+        return response()->download(public_path(Storage::url($article->image)),
+            $article->title);
+    }
+
     public function store(Request $request)
     {
-        $request->validate(self::$rules, self::$messages);
-        $article = Article::create($request->all());
-        return response()->json($article, 201);
+        $request->validate([
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id',
+            'image' => 'required|image|dimensions:min_width=200,min_height=200',
+        ], self::$messages);
+
+//        $article = Article::create($request->all());
+
+        $article = new Article($request->all());
+        $path = $request->image->store('public/articles');
+
+        $article->image = $path;
+        $article->save();
+
+        return response()->json(new ArticleResource($article), 201);
     }
+
     public function update(Request $request, Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,' . $article->id . '|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
         $article->update($request->all());
         return response()->json($article, 200);
     }
+
     public function delete(Article $article)
     {
         $article->delete();
